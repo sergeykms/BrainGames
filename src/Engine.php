cf09bcee1e24aaf978351154d7d8bf0c4fbeedf7
Popov Sergey <serpp@me.com>
2023-12-01 16:20:28 +1000

    correction autocheck step 6 correction Whitespace found at end of line function engine()

--- src/Engine.php
+++ src/Engine.php
@@ -15,7 +15,7 @@ use function cli\prompt;
 
 function engine(string $name, string $question, string $correctAnswer)
 {
-    line('Question: ' . $question); 
+    line('Question: ' . $question);
     $answer = prompt('Your answer');
     if ($correctAnswer !== $answer) {
         line("'" . $answer . "'" . " is wrong answer ;(. Correct answer was: " . "'" . $correctAnswer . "'");
